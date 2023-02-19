0472f5fe2385299c1de22203116c98dc0793f5cc
Lee-DoHa <johnny550@naver.com>
2023-02-19 23:25:42 +0900

    Update Item.js

--- begin-react/src/components/Item.js
+++ begin-react/src/components/Item.js
@@ -1,43 +1,26 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
-import { MdDone, MdDelete } from 'react-icons/md';
-
-const Remove = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #dee2e6;
-  font-size: 24px;
-  cursor: pointer;
-  &:hover {
-    color: #ff6b6b;
-  }
-  display: none;
-`;
+import { MdDone } from 'react-icons/md';
+import { useFixedDispatch } from './Context';
 
 const ItemBlock = styled.div`
   display: flex;
   align-items: center;
-  padding-top: 12px;
-  padding-bottom: 12px;
-  &:hover {
-    ${Remove} {
-      display: initial;
-    }
-  }
+  padding-top: 2px;
+  padding-bottom: 2px;
 `;
 
 const CheckCircle = styled.div`
-  width: 32px;
-  height: 32px;
-  border-radius: 16px;
+  width: 15px;
+  height: 15px;
+  border-radius: 15px;
   border: 1px solid #ced4da;
-  font-size: 24px;
+  font-size: 15px;
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-right: 20px;
-  cursor: pointer;
+  margin-right: 10px;
+  cursor: pointer; 
   ${props =>
     props.done &&
     css`
@@ -48,23 +31,24 @@ const CheckCircle = styled.div`
 
 const Text = styled.div`
   flex: 1;
-  font-size: 21px;
+  font-size: 15px;
   color: #495057;
   ${props =>
     props.done &&
     css`
-      color: #ced4da;
+      color: #20c997;
     `}
 `;
 
 function Item({ id, done, text }) {
+    const dispatch = useFixedDispatch();
+    const onToggle = () => dispatch({ type: 'TOGGLE', id})
   return (
     <ItemBlock>
-      <CheckCircle done={done}>{done && <MdDone />}</CheckCircle>
+      <CheckCircle done={done} onClick={onToggle}>
+        {done && <MdDone />}
+      </CheckCircle>
       <Text done={done}>{text}</Text>
-      <Remove>
-        <MdDelete />
-      </Remove>
     </ItemBlock>
   );
 }
