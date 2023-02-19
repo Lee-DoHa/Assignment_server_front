cc317b6fc69d96e17aaf20cabe48bc0480e06c39
Lee-DoHa <johnny550@naver.com>
2023-02-19 23:25:40 +0900

    Update Head.js

--- begin-react/src/components/Head.js
+++ begin-react/src/components/Head.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const HeadBlock = styled.div`
-  padding-top: 48px;
+  padding-top: 30px;
   padding-left: 32px;
   padding-right: 32px;
   padding-bottom: 24px;
@@ -12,15 +12,10 @@ const HeadBlock = styled.div`
     font-size: 36px;
     color: #343a40;
   }
-  .day {
-    margin-top: 4px;
-    color: #868e96;
-    font-size: 21px;
-  }
   .tasks-left {
     color: #20c997;
     font-size: 18px;
-    margin-top: 40px;
+    margin-top: 15px;
     font-weight: bold;
   }
 `;
@@ -29,8 +24,7 @@ function Head() {
   return (
     <HeadBlock>
       <h1>확장자 차단</h1>
-      <div className="day">차단할 확장자를 선택or직접 입력하세요</div>
-      <div className="tasks-left">doha</div>
+      <div className="tasks-left">차단할 확장자를 선택 or 직접 입력하세요</div>
     </HeadBlock>
   );
 }
