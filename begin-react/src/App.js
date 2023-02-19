2e0537c244d141a3b417346af82102b8137e8b46
Lee-DoHa <johnny550@naver.com>
2023-02-19 23:25:26 +0900

    Update App.js

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import AxiosApi from './AxiosApi';
 import { createGlobalStyle } from 'styled-components';
 import MainTemplate from './components/MainTemplate';
 import Head from './components/Head';
-import List from './components/List';
+import FixedList from './components/FixedList';
+import { FixedProvider } from './components/Context';
+
 
 
 const GlobalStyle = createGlobalStyle`
@@ -14,22 +15,68 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 function App() {
-  const name = 'react';
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24, // 기본 단위 px
-    padding: '1rem' // 다른 단위 사용 시 문자열로 설정
-  }
+  const [fileLimit, setFileLimit] = useState(null);
+
+  const fetchData = () => {
+    fetch('http://localhost:8080/fileLimit')
+      .then((Response) => Response.json())
+      .then((data) => setFileLimit(data));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    fetch('http://localhost:8080/fileLimit', { 
+      method: 'POST',
+      body: name,
+    }).then(() => fetchData());
+  };
+
+  const onClickHandler = (e) => {
+    e.preventDefault();
+    const id = e.target.id;
+    console.log(id);
+    fetch(`http://localhost:8080/fileLimit/${id}`, {
+      method: 'DELETE'
+    }).then(() => fetchData());
+  };
+
   return (
     <>
       <GlobalStyle />
       <MainTemplate>
         <Head />
-        <p>고정 확장자</p><input></input><br />
-        <p>선택 확장자</p><input></input><br />
-        <List />
-        <AxiosApi />
+        <h3 style={{paddingLeft: 20}}>고정 확장자</h3>
+        <FixedProvider>
+          <FixedList />
+        </FixedProvider>
+        <h3 style={{paddingLeft: 20}}>선택 확장자</h3>
+        <form onSubmit={onSubmitHandler}>
+          <input name='name'/>
+          <input type='submit' value='추가'/>
+        </form>
+        <div style={{ display: 'grid',gridTemplateColumns: 'repeat(5, 1fr)'}}>
+          {fileLimit?.filter((limit) => limit.type === 'CUSTOM').map((limit) => (
+            <div key={limit.id} 
+              style={{
+                display: 'grid',
+                gridTemplateColumns: 'repeat(2, 1fr)',
+                border: '1px solid black',
+                padding: '5px',
+                margin: '2px',
+                borderRadius: '5px',
+                }}
+              >
+              <div>{limit.name}</div>
+              <button onClick={onClickHandler} id={limit.id}>X</button>
+            </div>
+            
+          ))}
+        </div>
       </MainTemplate> 
       
     </>
